7b0171ca2b7fd7f26e01133f8536293f69314ebb
José Ruiz <143463950+JoseManuelNunezRuiz@users.noreply.github.com>
2024-07-19 20:56:09 -0600

    Update analisis_tiempo_real.py

--- analisis_tiempo_real.py
+++ analisis_tiempo_real.py
@@ -1,22 +1,26 @@
 from kafka import KafkaProducer
 import json
 import tweepy
+from pyflink.datastream import StreamExecutionEnvironment
+from pyflink.datastream.connectors import FlinkKafkaConsumer
+from pyflink.common.serialization import SimpleStringSchema
+import threading
 
-# Configuración de Twitter API
+# Configuración API
 consumer_key = 'consumer_key'
 consumer_secret = 'consumer_secret'
 access_token = 'access_token'
 access_token_secret = 'access_token_secret'
 
-# Configuración de Kafka
+# Configuración Kafka
 kafka_topic = 'tweets_topic'
 kafka_bootstrap_servers = 'localhost:9092'
 
-# Productor de Kafka
+# Configurar productor Kafka
 producer = KafkaProducer(bootstrap_servers=kafka_bootstrap_servers,
                          value_serializer=lambda v: json.dumps(v).encode('utf-8'))
 
-# Cliente de Twitter
+# Configurar cliente Twitter
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
@@ -31,7 +35,39 @@ class MyStreamListener(tweepy.StreamListener):
         producer.send(kafka_topic, tweet)
         print(f"Tweet enviado a Kafka: {tweet}")
 
-# Stream de Twitter
-stream_listener = MyStreamListener()
-stream = tweepy.Stream(auth=api.auth, listener=stream_listener)
-stream.filter(track=['python', 'data'])
+def start_twitter_stream():
+    stream_listener = MyStreamListener()
+    stream = tweepy.Stream(auth=api.auth, listener=stream_listener)
+    stream.filter(track=['python', 'data'], is_async=True)
+
+def process_tweets():
+    env = StreamExecutionEnvironment.get_execution_environment()
+
+    # Configuración consumidor Kafka
+    kafka_props = {
+        'bootstrap.servers': 'localhost:9092',
+        'group.id': 'flink_consumer_group'
+    }
+
+    kafka_consumer = FlinkKafkaConsumer(
+        topics='tweets_topic',
+        deserialization_schema=SimpleStringSchema(),
+        properties=kafka_props
+    )
+
+    # Leer mensajes Kafka
+    stream = env.add_source(kafka_consumer)
+
+    # Procesar tweets
+    stream.map(lambda tweet: f"Procesando tweet: {tweet}") \
+          .print()
+
+    env.execute("Procesamiento de Tweets en Tiempo Real")
+
+if __name__ == '__main__':
+    # Iniciar el stream de Twitter en un hilo separado
+    twitter_stream_thread = threading.Thread(target=start_twitter_stream)
+    twitter_stream_thread.start()
+
+    # Procesar tweets en tiempo real con Flink y Kafka
+    process_tweets()
